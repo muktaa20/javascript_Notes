4094d328f98a6a75fa25b01bbbf7a22d71ab6516
Mukta Suryavanshi <tulsiramsuryavanshi956@gmail.com>
2025-09-18 16:59:08 +0530

    Added all JavaScript notes and code

---
+++ array/methods.js
@@ -0,0 +1,15 @@
+            //   ARRAY METHODS
+let arr = [4, 7, 8, 0, 10, 16, 2]
+// console.log(arr.length)   //it will retuen the length of array 7
+
+// arr.pop()  //it will return the last element of array 2
+// console.log(arr);
+
+// console.log(arr.pop());  //it will return the poped value
+
+// arr.unshift(-3) // it will add this value in the front of array
+// console.log(arr);
+
+// arr.shift()  //it will remove first value of array
+// console.log(arr);
+
